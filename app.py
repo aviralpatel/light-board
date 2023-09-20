06f3c3ad82b6ed9969a95c5485711a9a4e2a10c8
AviralPatel <aviralpatel12379@icloud.com>
2023-09-21 01:48:40 +0530

    made few changes

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ import cv2
 import numpy as np
 from utility import compressor, lessFrame, brightestSubsetMatrix
 
-state = True
 stream = cv2.VideoCapture(1) #use 0 for default webcam
+
 while True:
     ret, frame = stream.read()
-    if state and ret:
-        cv2.waitKey(10)
-        state = not state
+
     monoColorFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #convert to greyscale
     monoColorFrame = cv2.flip(monoColorFrame, 1) #flip the captured frame
+
     compressedFrame = lessFrame(monoColorFrame, 5) #compress the frame and reduce the resolution by 25 for faster processing
+
     co_ordinates = brightestSubsetMatrix(compressedFrame, 10) #get co-ordinates
+
     cv2.imshow("monoclorImg", compressedFrame)
+
     if len(co_ordinates) > 2:
         print(f"x = {co_ordinates[1]*5} , y = {co_ordinates[0]*5}") #print the co-ordinates
         # put further code inside this if statement
